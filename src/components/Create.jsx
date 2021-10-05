8dc6b90f7054e1eec292a4c82aed21384b0fa096
Chintan Patel <chihempat@gmail.com>
2021-10-05 18:48:08 +0530

    commits

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
 
 function Create() {
+  const options = ["Select One", "Select Muiltiple"];
+  const defaultOption = options[0];
+
   return (
     <div className="container">
-      <input type="dropDown" />
+      <Dropdown
+        options={options}
+        onChange={this._onSelect}
+        value={defaultOption}
+        placeholder="Select an option"
+      />
+      {defaultOption === "Select One" ? <div></div> : <div></div>}
     </div>
   );
 }
